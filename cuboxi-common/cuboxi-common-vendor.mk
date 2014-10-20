d661d16e93f55b41e0307f6511e27c2044a0889b
Humberto Borba <humberos@gmail.com>
2014-10-20 02:18:45 -0200

    organizing imx6 blobs
    moved blobs from device repos
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I9b514fa5f2a50c6ebb967f02043b4a6c6a99f061

--- cuboxi-common/cuboxi-common-vendor.mk
+++ cuboxi-common/cuboxi-common-vendor.mk
@@ -20,46 +20,57 @@ PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/bootloader/u-boot.img:u-boot.img \
     $(VENDOR_PATH)/bootloader/SPL:SPL
 
-# WLAN
-# 4329 and 4330 firmware files
+# Bluetooth
+# 4329 and 4330
 PRODUCT_COPY_FILES += \
-    $(VENDOR_PATH)/brcm/bcm4329_fw.bin:system/etc/firmware/brcm/brcmfmac4329-sdio.bin \
-    $(VENDOR_PATH)/brcm/bcm4329_nvram.txt:system/etc/firmware/brcm/brcmfmac4329-sdio.txt \
-    $(VENDOR_PATH)/brcm/bcm4329.hcd:system/etc/firmware/brcm/BCM4329B1.hcd \
-    $(VENDOR_PATH)/brcm/bcm4330_fw.bin:system/etc/firmware/brcm/brcmfmac4330-sdio.bin \
-    $(VENDOR_PATH)/brcm/bcm4330_nvram.txt:system/etc/firmware/brcm/brcmfmac4330-sdio.txt \
-    $(VENDOR_PATH)/brcm/bcm4330.hcd:system/etc/firmware/brcm/BCM4330.hcd
+    $(VENDOR_PATH)/brcm/bcm4329.hcd:system/vendor/firmware/brcm/BCM4329B1.hcd \
+    $(VENDOR_PATH)/brcm/bcm4330.hcd:system/vendor/firmware/brcm/BCM4330.hcd
 
-# GPU and HW composer
-# Vivante
+# WLAN
+# 4329 and 4330
 PRODUCT_COPY_FILES += \
-    $(VENDOR_PATH)/gpu/egl/egl.cfg:system/lib/egl/egl.cfg \
-    $(VENDOR_PATH)/gpu/egl/libEGL_VIVANTE.so:system/lib/egl/libEGL_VIVANTE.so \
-    $(VENDOR_PATH)/gpu/egl/libGLESv1_CM_VIVANTE.so:system/lib/egl/libGLESv1_CM_VIVANTE.so \
-    $(VENDOR_PATH)/gpu/egl/libGLESv2_VIVANTE.so:system/lib/egl/libGLESv2_VIVANTE.so \
-    $(VENDOR_PATH)/gpu/libGAL.so:system/lib/libGAL.so \
-    $(VENDOR_PATH)/gpu/libGAL.so:obj/lib/libGAL.so \
-    $(VENDOR_PATH)/gpu/libg2d.so:system/lib/libg2d.so \
-    $(VENDOR_PATH)/gpu/libg2d.so:obj/lib/libg2d.so \
-    $(VENDOR_PATH)/gpu/libGLSLC.so:system/lib/libGLSLC.so \
-    $(VENDOR_PATH)/gpu/hw/gralloc_viv.imx6.so:system/lib/hw/gralloc_viv.imx6.so \
-    $(VENDOR_PATH)/gpu/hw/hwcomposer_viv.imx6.so:system/lib/hw/hwcomposer_viv.imx6.so
+    $(VENDOR_PATH)/brcm/bcm4329_fw.bin:system/vendor/firmware/brcm/brcmfmac4329-sdio.bin \
+    $(VENDOR_PATH)/brcm/bcm4329_nvram.txt:system/vendor/firmware/brcm/brcmfmac4329-sdio.txt \
+    $(VENDOR_PATH)/brcm/bcm4330_fw.bin:system/vendor/firmware/brcm/brcmfmac4330-sdio.bin \
+    $(VENDOR_PATH)/brcm/bcm4330_nvram.txt:system/vendor/firmware/brcm/brcmfmac4330-sdio.txt
+
+# GPU
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/gpu/egl.cfg:system/lib/egl/egl.cfg \
+    $(VENDOR_PATH)/gpu/libEGL_VIVANTE.so:system/lib/egl/libEGL_VIVANTE.so \
+    $(VENDOR_PATH)/gpu/libGLESv1_CM_VIVANTE.so:system/lib/egl/libGLESv1_CM_VIVANTE.so \
+    $(VENDOR_PATH)/gpu/libGLESv2_VIVANTE.so:system/lib/egl/libGLESv2_VIVANTE.so
+
+# hwcomposer and gralloc
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/hw/gralloc_viv.imx6.so:system/lib/hw/gralloc_viv.imx6.so \
+    $(VENDOR_PATH)/hw/hwcomposer_viv.imx6.so:system/lib/hw/hwcomposer_viv.imx6.so \
+    $(VENDOR_PATH)/hw/hwcomposer_fsl.imx6.so:system/lib/hw/hwcomposer_fsl.imx6.so
+
+# libg2d
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/libg2d/libGAL.so:system/lib/libGAL.so \
+    $(VENDOR_PATH)/libg2d/libGAL.so:obj/lib/libGAL.so \
+    $(VENDOR_PATH)/libg2d/libg2d.so:system/lib/libg2d.so \
+    $(VENDOR_PATH)/libg2d/libg2d.so:obj/lib/libg2d.so \
+    $(VENDOR_PATH)/libg2d/libGLSLC.so:system/lib/libGLSLC.so
+
+PRODUCT_COPY_FILES += \
+    external/linux-firmware-imx/firmware/vpu/vpu_fw_imx6d.bin:system/lib/firmware/vpu/vpu_fw_imx6d.bin \
+    external/linux-firmware-imx/firmware/vpu/vpu_fw_imx6q.bin:system/lib/firmware/vpu/vpu_fw_imx6q.bin
 
 # Ril
 # Prebuilt lib ril
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/ril/libreference-ril-zte.so:system/lib/libreference-ril-zte.so
 
-# Sensor
-# Mag
-PRODUCT_COPY_FILES += \
-    $(VENDOR_PATH)/sensor/magd:system/bin/magd
-
-# VPU test binaries
+# VPU
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/vpu/gmem_info:system/bin/gmem_info \
     $(VENDOR_PATH)/vpu/mxc_ipudev_test:system/bin/mxc_ipudev_test \
-    $(VENDOR_PATH)/vpu/mxc_vpu_test:system/bin/mxc_vpu_test
+    $(VENDOR_PATH)/vpu/mxc_vpu_test:system/bin/mxc_vpu_test \
+    $(VENDOR_PATH)/vpu/vpu_fw_imx6d.bin:system/vendor/firmware/vpu/vpu_fw_imx6d.bin \
+    $(VENDOR_PATH)/vpu/vpu_fw_imx6q.bin:system/vendor/firmware/vpu/vpu_fw_imx6q.bin
 
 # Terminal
 PRODUCT_COPY_FILES += \
